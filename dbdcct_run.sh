928dabde4e0fdb2d86b8362794946b7b72a54812
amacomber <68449783+amacomber@users.noreply.github.com>
2024-04-06 13:29:34 -0600

    Update dbdcct_run.sh
    Need to update class attributes

--- dbdcct_run.sh
+++ dbdcct_run.sh
@@ -5,7 +5,6 @@ currentUser=$(stat -f %Su /dev/console)
 prefList="com.goteamgecko.tabletop.game.plist"
 setChk="/Users/$currentUser/Library/Preferences/$prefList"
 
-
 # Check if plist exists and if not, create it
 if [ ! -f "$setChk" ] && touch "$setChk"
 
@@ -35,6 +34,7 @@ else
         defaults write "$setChk" newGame -int 1
         defaults write "$setChk" setDone -int 0
         defaults write "$setChk" basePoints -int 12
+        defaults write "$setChk" usedPoints -int 0
 
     fi
 
@@ -76,7 +76,6 @@ avatarName=$(defaults read "$setChk" avatarName)
         defaults write "$setChk" avatarName -string "$avatarName"
         encodedAvatar=$(echo "$avatarName" | base64)
         defaults write "$setChk" encodedAvatar "$encodedAvatar"
-        defaults write "$setChk" usedPoints -int 0
     fi
 else
 echo "Thanks for returning, $userName, your avatar, $avatarName, is ready to set up"
@@ -95,19 +94,31 @@ myRace=$(defaults read "$setChk" myRace)
 ### Check Avatar Race
 if [[ -z "$myRace" ]]; then
 ### Open a dialog box and ask the user for their avatar's race
-myRace=$(osascript -e 'tell application "System Events" to choose from list {"Human", "Elf", "Draco", "Titon"} with prompt "What Race will you be?"')
+myRace=$(osascript -e 'tell application "System Events" to choose from list {"Human", "Elf", "Draco", "Titon"} with prompt "What race will you be?"')
 defaults write "$setChk" myRace -string "$myRace"
 else
     echo "You selected: $myRace"
 fi
 
-### Define Class name
+### Define Avatar Gender
+myRace=$(defaults read "$setChk" myRace)
+
+### Check Avatar Gender
+if [[ -z "$myGender" ]]; then
+### Open a dialog box and ask the user for their avatar's gender
+myRace=$(osascript -e 'tell application "System Events" to choose from list {"Non Binary", "Female", "Male"} with prompt "What gender will you be?"')
+defaults write "$setChk" myGender -string "$myGender"
+else
+    echo "You selected: $myGender"
+fi
+
+### Define Avatar Class
 myClass=$(defaults read "$setChk" myClass)
 
 ### Choose Class
 if [[ -z "$myClass" ]]; then
 ### Open a dialog box and ask the user for their avatar's class
-myClass=$(osascript -e 'tell application "System Events" to choose from list {"Warrior", "Thief", "Archer", "Magika"} with prompt "What Class will you be?"')
+myClass=$(osascript -e 'tell application "System Events" to choose from list {"Warrior", "Thief", "Archer", "Magika"} with prompt "What class will you be?"')
 defaults write "$setChk" myClass -string "$myClass"
 else
 if [[ "$myClass" == "false" ]]; then
@@ -121,26 +132,26 @@ fi
     fi
 
 ### Award base points
-if [ $(defaults read "$setChk" basePoints) -eq 0 ]; then
-if [ $myRace == "Human" ];
+if [ "$(defaults read "$setChk" basePoints)" -eq 0 ]; then
+if [ "$myRace" == "Human" ];
 then
 ### Apply human base points
 defaults write "$setChk" basePoints -int 14
 echo "14 base points added for $myRace race"
 fi
-if [ $myRace == "Elf" ];
+if [ "$myRace" == "Elf" ];
 then
 ### Apply elf base points
 defaults write "$setChk" basePoints -int 13
 echo "13 base points added for $myRace race"
 fi
-if [ $myRace == "Draco" ];
+if [ "$myRace" == "Draco" ];
 then
 ### Apply draco base points
 defaults write "$setChk" basePoints -int 15
 echo "15 base points added for $myRace race"
 fi
-if [ $myRace == "Titon" ];
+if [ "$myRace" == "Titon" ];
 then
 ## Apply titon base points
 defaults write "$setChk" basePoints -int 14
@@ -151,29 +162,29 @@ echo "Base points already awarded"
 fi
 
 ### Set raceCodes
-if [ -z $(defaults read "$setChk" raceCode) ]; then
-if [ $myRace == "Human" ];
+if [ -z "$(defaults read "$setChk" raceCode)" ]; then
+if [ "$myRace" == "Human" ];
 then
 ### Apply human raceCode for points table
 defaults write "$setChk" raceCode -string 01
 raceCode=$(defaults read "$setChk" raceCode)
 echo "raceCode: $raceCode"
 fi
-if [ $myRace == "Elf" ];
+if [ "$myRace" == "Elf" ];
 then
 ### Apply elf raceCode for points table
 defaults write "$setChk" raceCode -string 02
 raceCode=$(defaults read "$setChk" raceCode)
 echo "raceCode: $raceCode"
 fi
-if [ $myRace == "Draco" ];
+if [ "$myRace" == "Draco" ];
 then
 ### Apply draco raceCode for points table
 defaults write "$setChk" raceCode -string 03
 raceCode=$(defaults read "$setChk" raceCode)
 echo "raceCode: $raceCode"
 fi
-if [ $myRace == "Titon" ];
+if [ "$myRace" == "Titon" ];
 then
 ### Apply titon raceCode for points table
 defaults write "$setChk" raceCode -string 04
@@ -184,7 +195,92 @@ else
 echo "raceCode already set"
 fi
 
-### Apply Class Points
+### Set classCode
+if [ -z "$(defaults read "$setChk" classCode)" ]; then
+if [ "$myClass" == "Warrior" ];
+then
+### Apply warrior classCode for points table
+defaults write "$setChk" classCode -string 01
+classCode=$(defaults read "$setChk" classCode)
+echo "classCode: $classCode"
+fi
+if [ "$myClass" == "Thief" ];
+then
+### Apply thief classCode for points table
+defaults write "$setChk" classCode -string 02
+classCode=$(defaults read "$setChk" classCode)
+echo "classCode: $classCode"
+fi
+if [ "$myClass" == "Archer" ];
+then
+### Apply archer classCode for points table
+defaults write "$setChk" classCode -string 03
+classCode=$(defaults read "$setChk" classCode)
+echo "classCode: $classCode"
+fi
+if [ "$myClass" == "Magika" ];
+then
+### Apply magika classCode for points table
+defaults write "$setChk" classCode -string 04
+classCode=$(defaults read "$setChk" classCode)
+echo "classCode: $classCode"
+fi
+else
+echo "classCode already set"
+fi
+
+### Set classAttrib
+if [ -z "$(defaults read "$setChk" classAttrib)" ]; then
+if [ "$myClass" == "Warrior" ];
+then
+### Apply warrior classAttrib for points table
+defaults write "$setChk" classAttribAtt -int 4
+defaults write "$setChk" classAttribStr -int 4
+defaults write "$setChk" classAttribSte -int 1
+defaults write "$setChk" classAttribSub -int 2
+defaults write "$setChk" classAttribRan -int 1
+defaults write "$setChk" classAttribCom -int 2
+defaults write "$setChk" classAttribMag -int 2
+echo "$myClass attributes set"
+fi
+if [ "$myClass" == "Thief" ];
+then
+### Apply thief classAttrib for points table
+defaults write "$setChk" classAttribAtt -int 4
+defaults write "$setChk" classAttribStr -int 4
+defaults write "$setChk" classAttribSte -int 1
+defaults write "$setChk" classAttribSub -int 2
+defaults write "$setChk" classAttribRan -int 1
+defaults write "$setChk" classAttribCom -int 2
+defaults write "$setChk" classAttribMag -int 2
+echo "$myClass attributes set"
+fi
+if [ "$myClass" == "Archer" ];
+then
+### Apply warrior classAttrib for points table
+defaults write "$setChk" classAttribAtt -int 4
+defaults write "$setChk" classAttribStr -int 4
+defaults write "$setChk" classAttribSte -int 1
+defaults write "$setChk" classAttribSub -int 2
+defaults write "$setChk" classAttribRan -int 1
+defaults write "$setChk" classAttribCom -int 2
+defaults write "$setChk" classAttribMag -int 2
+echo "$myClass attributes set"
+fi
+if [ "$myClass" == "Magika" ];
+then
+### Apply warrior classAttrib for points table
+defaults write "$setChk" classAttribAtt -int 4
+defaults write "$setChk" classAttribStr -int 4
+defaults write "$setChk" classAttribSte -int 1
+defaults write "$setChk" classAttribSub -int 2
+defaults write "$setChk" classAttribRan -int 1
+defaults write "$setChk" classAttribCom -int 2
+defaults write "$setChk" classAttribMag -int 2
+echo "$myClass attributes set"
+fi
+defaults write "$setChk" classAttrib -int 1
+fi
 
 
 
